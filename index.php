da260430b8edeaf610768a83c587a143d295b8c4
axel <axl.78@hotmail.fr>
2014-10-22 12:19:16 +0200

    Remove slugify

--- index.php
+++ index.php
@@ -6,8 +6,4 @@
  * Time: 17:45
  */
 require __DIR__.'/vendor/autoload.php';
-$name = 'qsjbdfdsk:fbshfbdhgdhgdhvsqdhfbzUFEGZIMEO34567U43é"&&&&  âêî  ééé""é&"';
 
-$slugify = new \Cocur\Slugify\Slugify();
-
-echo $slugify->slugify($name);
