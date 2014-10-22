6dc4eb18d228c49ff5b646f109fffcc7d1a1a75f
axel <axl.78@hotmail.fr>
2014-10-22 13:00:50 +0200

    Add views

--- index.php
+++ index.php
@@ -7,3 +7,23 @@
  */
 require __DIR__.'/vendor/autoload.php';
 
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+
+]);
+
+
+$article  = [
+    'name' => 'zefefefe',
+    'content' => 'dsqfjsi',
+
+
+];
+
+$twig = new Twig_Environment($loader,[
+    //'cache' => null,
+    ]);
+echo $twig->render('article.html.twig', [
+'article' => $article,
+]);
